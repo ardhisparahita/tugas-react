09f550a8cddfe47232068b360e55eb05cf02467c
ardhisparahita <92034363+ardhisparahita@users.noreply.github.com>
2022-10-27 23:27:21 +0700

    create components text

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Card from './Components/Card/Card';
+import Text from './Components/Text/Text';
 
 function App() {
   return (
     <div className="App">
       <Card/>
+      <Text/>
     </div>
   );
 }
---
+++ src/Components/Text/Text.jsx
@@ -0,0 +1,13 @@
+import styles from "./Text.module.css"
+
+const Text = () => {
+    return (
+        <>
+            <div className={styles.text}>
+                <h1>Ini Text</h1>
+            </div>
+        </>
+    )
+}
+
+export default Text
