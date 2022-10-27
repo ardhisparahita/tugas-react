624d4bcc8dc426f1ded442899ecdd79c4723f1f0
ardhisparahita <92034363+ardhisparahita@users.noreply.github.com>
2022-10-27 23:21:15 +0700

    create components card

---
+++ src/Components/Card/Card.jsx
@@ -0,0 +1,15 @@
+import styles from "./Card.module.css"
+
+const Card = () => {
+    return (
+        <>
+            <div className={styles.card}>
+                <h1>
+                    Ini Card
+                </h1>
+            </div>
+        </>
+    )
+}
+
+export default Card
