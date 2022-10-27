fccac05247d330bca9a9b892d1667ee7c3c09a89
ardhisparahita <92034363+ardhisparahita@users.noreply.github.com>
2022-10-27 23:31:59 +0700

    create components button

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Card from './Components/Card/Card';
 import Text from './Components/Text/Text';
+import Button from './Components/Button/Button';
 
 function App() {
   return (
     <div className="App">
       <Card/>
       <Text/>
+      <Button/>
     </div>
   );
 }
---
+++ src/Components/Button/Button.jsx
@@ -0,0 +1,13 @@
+import styles from "./Button.module.css"
+
+const Button = () => {
+    return (
+        <>
+            <div className={styles.button}>
+                <h1>Ini button</h1>
+            </div>
+        </>
+    )
+}
+
+export default Button
